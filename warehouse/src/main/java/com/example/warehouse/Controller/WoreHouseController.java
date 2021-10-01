6bd27fe5223e296e212aaa6ac3560459cb6509f5
Chamika Deshan Jayasingha <56592121+ChamikaD97@users.noreply.github.com>
2021-10-01 18:36:18 +0530

    Update WoreHouseController.java

--- warehouse/src/main/java/com/example/warehouse/Controller/WoreHouseController.java
+++ warehouse/src/main/java/com/example/warehouse/Controller/WoreHouseController.java
@@ -5,7 +5,6 @@ import com.example.warehouse.Service.StoreItemTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
-
 import java.awt.*;
 import java.util.List;
 
