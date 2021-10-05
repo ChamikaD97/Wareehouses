a5463b6729b0934462b850a5bcf3e7a862841b0c
Chamika Deshan Jayasingha <56592121+ChamikaD97@users.noreply.github.com>
2021-10-06 01:22:12 +0530

    Update WarehouseApplicationTests.java

--- warehouse/src/test/java/com/example/warehouse/WarehouseApplicationTests.java
+++ warehouse/src/test/java/com/example/warehouse/WarehouseApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.warehouse;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 class WarehouseApplicationTests {
 
